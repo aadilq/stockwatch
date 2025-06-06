1165a31bae0a4c82457ebf08ace2246159767fdd
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-06-06 17:35:59 -0400

    Sucessfully connect to MONGODB database

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT;
+
+const URI = process.env.MONGO_DB_URI
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(URI, {
+        });
+        console.log('Database is connected');
+    } catch (err) {
+        console.error('Error connecting to the database:', err);
+        process.exit(1);
+    }
+};
+
+connectDB();
+
+
 
 app.listen(PORT, (error) =>{
     if(!error)
@@ -11,3 +29,4 @@ app.listen(PORT, (error) =>{
         console.log("Error occurred, server can't start", error);
     }
 );
+
