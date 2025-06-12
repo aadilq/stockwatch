42208ca2d95d3251702d43965c2e145909a31b49
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-06-11 21:57:27 -0400

    create react component to display list of stocks

--- stockwatch-frontend/src/App.jsx
+++ stockwatch-frontend/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	NavLink,
+} from "react-router-dom";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Stocks = ({addToWatchList}) => {
+  const[stocks, setStocks] = useState([]);
+  const [dataIsLoaded, setDataIsLoaded] = useState(false);
 
-  return (
-    <>
+  useEffect(() => {
+    //We want to fetch the data from our backend server
+    fetch("http://localhost:5000/api/stocks")
+    .then((res) => res.json())
+    .then((data) => setStocks(data))
+    .catch((error) => console.error("Error fetching stocks:", error));
+    setDataIsLoaded(true);
+  }, [])
+
+  if(!dataIsLoaded){
+    return(
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Please wait some time...</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )
+  }
+
+  const getRandomColors = () =>{
+    const colors = ["FF3F33", "#075B5E"]
+    return colors[Math.floor(Math.random() * colors.length)]
+  };
+
+  return(
+    <div className="App">
+      <h1>Stockwatch</h1>
+      <h2>Stocks</h2>
+      <ul>
+        {stocks.map((stock) => (
+          <li key={stock.symbol}>
+            {stock.company} ({stock.symbol}) - 
+            <span style={{color: getRandomColors() }}>
+              {" "}
+              ${stock.initial_price}
+            </span>
+            <button onClick={() => addToWatchList(stock)}>
+              Add to my Watchlist
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default App
+export default Stocks;
